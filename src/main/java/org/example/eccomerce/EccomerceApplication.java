39104101f52853f0936648e172203b75bb84481d
thisishwan2 <feel000617@gmail.com>
2024-01-03 16:17:16 +0900

    feat: project setting(service-discovery)

---
+++ src/main/java/org/example/eccomerce/EccomerceApplication.java
@@ -0,0 +1,15 @@
+package org.example.eccomerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EccomerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EccomerceApplication.class, args);
+    }
+
+}
